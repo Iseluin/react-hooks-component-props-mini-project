5db0ef9058a46842e22f1be5a2e9b35b7c9a8a7f
iseluin <kutay.kagan.ozenn@gmail.com>
2021-07-25 16:39:57 +0300

    Done.

---
+++ src/components/ArticleList.js
@@ -0,0 +1,52 @@
+// import React from "react";
+// import Article from "./Article";
+
+// const ArticleList = ({ articles }) => {
+//     const posts = articles.map((article) => (
+//         <Article key={article.id} title={article.title} date={article.date} preview={article.preview} minutes={article.minutes} />
+//     ))
+
+//     return (
+//         <main>
+//             {posts}
+//         </main>
+//     )
+// }
+
+// export default ArticleList;
+
+// import React from "react";
+// import Article from "./Article";
+
+// function ArticleList({ articles }) {
+//   const posts = articles.map((article) => (
+//     <Article
+//       key={article.id}
+//       title={article.title}
+//       date={article.date}
+//       preview={article.preview}
+//       minutes={article.minutes}
+//     />
+//   ));
+//   return <main>{posts}</main>;
+// }
+
+// export default ArticleList;
+
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+  const articles = posts.map((post) => (
+    <Article
+      key={post.id}
+      title={post.title}
+      date={post.date}
+      preview={post.preview}
+      minutes={post.minutes}
+    />
+  ));
+  return <main>{articles}</main>;
+}
+
+export default ArticleList;
